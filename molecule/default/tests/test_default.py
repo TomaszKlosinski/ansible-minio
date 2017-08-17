93850b47eca7b6657405c5a75fb4d4861bce67d9
Tomasz Klosinski <tkl@scaling.ch>
2017-08-17 11:17:05 +0200

    Add molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,33 @@
 import os
+import yaml
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+@pytest.fixture()
+def AnsibleDefaults(Ansible):
+    with open("./defaults/main.yml", 'r') as stream:
+        return yaml.load(stream)
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
 
+@pytest.mark.parametrize('minio_bin_var', [
+    'minio_server_bin_path',
+    'minio_client_bin_path',
+])
+def test_minio_installed(File, AnsibleDefaults, minio_bin_var):
+
+    f = File(AnsibleDefaults[minio_bin_var])
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == AnsibleDefaults['minio_user']
+    assert f.group == AnsibleDefaults['minio_group']
+    assert oct(f.mode) == '0755'
+
+
+def test_minio_service(Service):
+
+    s = Service('minio')
+    assert s.is_running
+    assert s.is_enabled
